d93c69de4614af220a8d98d0a33c3c09946aefb9
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-06-06 13:28:03 +0200

    chore: init flake (devshell only for now)
    Signed-off-by: Gwenn Le Bihan <gwenn.lebihan7@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Mock communications with h1's low-level rt/lowstate and rt/lowcmd DDS channels, as sent and received by  Unitree's SDK2 (https://github.com/unitreerobotics/unitree_sdk2).";
+
+  inputs = {
+    # TODO: drop `/module` after https://github.com/Gepetto/nix/pull/54
+    gepetto.url = "github:gepetto/nix/module";
+    flake-parts.follows = "gepetto/flake-parts";
+    nixpkgs.follows = "gepetto/nixpkgs";
+    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
+    systems.follows = "gepetto/systems";
+    treefmt-nix.follows = "gepetto/treefmt-nix";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [ inputs.gepetto.flakeModule ];
+      perSystem =
+        {
+          lib,
+          pkgs,
+          self',
+          ...
+        }:
+        {
+	  devShells.default = pkgs.mkShell {
+	    buildInputs = with pkgs.python3Packages; [ cyclonedds-python ];
+	  };
+        };
+    };
+}
