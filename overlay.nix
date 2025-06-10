f4ad3cef0bd74801d84db3d48dbdc155390933d4
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-06-10 17:44:17 +0200

    clean

--- overlay.nix
+++ overlay.nix
@@ -1,11 +1,11 @@
-final: prev: {
+final: prev:
+{
 
   # unitree sdk *really* wants 0.10.2
-  cyclonedds = prev.cyclonedds.overrideAttrs (_super: rec {
+  cyclonedds = prev.cyclonedds.overrideAttrs (super: rec {
     version = "0.10.2";
     src = final.fetchFromGitHub {
-      owner = "eclipse-cyclonedds";
-      repo = "cyclonedds";
+      inherit (super.src) owner repo;
       tag = version;
       hash = "sha256-xr9H9n+gyFMgEMHn59T6ELYVZJ1m8laG0d99SE9k268=";
     };
@@ -16,15 +16,14 @@ final: prev: {
       python-final: python-prev:
       {
         # unitree sdk *really* wants 0.10.2
-        cyclonedds-python = python-prev.cyclonedds-python.overrideAttrs {
+        cyclonedds-python = python-prev.cyclonedds-python.overrideAttrs (super: rec {
           inherit (final.cyclonedds) version;
           src = final.fetchFromGitHub {
-            owner = "eclipse-cyclonedds";
-            repo = "cyclonedds-python";
-            tag = final.cyclonedds.version;
+            inherit (super.src) owner repo;
+            tag = version;
             hash = "sha256-EnvNWIYDviMPANanUqI7Uk8lQBjXcN9DVb9OZlAelrM=";
           };
-        };
+        });
       }
       // final.lib.filesystem.packagesFromDirectoryRecursive {
         inherit (python-final) callPackage;
@@ -32,8 +31,9 @@ final: prev: {
       }
     )
   ];
-} 
-      // final.lib.filesystem.packagesFromDirectoryRecursive {
-        inherit (final) callPackage;
-        directory = ./pkgs;
-      }
+}
+# infinite recursion with final ?
+// prev.lib.filesystem.packagesFromDirectoryRecursive {
+  inherit (final) callPackage;
+  directory = ./pkgs;
+}
